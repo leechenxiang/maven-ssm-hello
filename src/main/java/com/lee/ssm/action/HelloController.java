be8e27ffe8a7b0debf1583e963576665b2c71c53
李晨翔 <leechenxiang@163.com>
2016-03-27 20:29:43 +0800

    add commonts

--- src/main/java/com/lee/ssm/action/HelloController.java
+++ src/main/java/com/lee/ssm/action/HelloController.java
@@ -9,13 +9,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.lee.ssm.bean.User;
 import com.lee.ssm.service.IUserService;
 
+/**
+ * 
+ * @Title: HelloController.java
+ * @Package com.lee.ssm.action
+ * @Description: TODO
+ * Copyright: Copyright (c) 2016
+ * Company:Nathan.Lee.Salvatore
+ * 
+ * @author leechenxiang
+ * @date 2016年3月25日 下午10:34:51
+ * @version V1.0
+ */
 @Controller
 @RequestMapping("/root")
 public class HelloController {
 	
 	@Resource
 	private IUserService userService;
-	
+
+	/**
+	 * 
+	 * @Description: TODO
+	 * @param model
+	 * @return
+	 * @throws Exception
+	 * 
+	 * @author leechenxiang
+	 * @date 2016年3月25日 下午10:36:49
+	 */
 	@RequestMapping(value = "/hello")
 	public String home(Model model) throws Exception {
 		
